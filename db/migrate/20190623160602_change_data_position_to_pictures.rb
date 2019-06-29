5140dbf240873477f8a7cd40de3155ec4f3ef90f
nakao <kouki.2033922@gmail.com>
2019-06-29 10:19:56 +0000

    migrate heroku erorr

--- db/migrate/20190623160602_change_data_position_to_pictures.rb
+++ db/migrate/20190623160602_change_data_position_to_pictures.rb
@@ -1,5 +1,9 @@
 class ChangeDataPositionToPictures < ActiveRecord::Migration[5.2]
   def change
-    change_column :pictures, :position, :integer
+    if Rails.env.development? || Rails.env.test?
+      change_column :pictures, :position, :integer 
+    else Rails.env.production?
+      change_column :pictures, :position, 'integer USING CAST(position AS integer)'
+    end
   end
 end
