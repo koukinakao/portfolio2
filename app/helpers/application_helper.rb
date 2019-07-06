81c0c17f829d1cd7f95655047b747fc963258dd7
nakao <kouki.2033922@gmail.com>
2019-07-06 14:43:33 +0000

    volume-show-average

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def evalu_average(evalu)
     evalu.evaluations.average(:star).round(1)
   end
+  
+  def evalu_average(book)
+    book.evaluations.average(:star).round(1)
+  end
 end
